ee1550c29d24e977120c5443645cc3496b9cb244
Jose Benavides <jbenavides.bermudez@gmail.com>
2016-10-29 23:40:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jbenavides_pc/interactivestory/model/Choice.java
@@ -0,0 +1,32 @@
+package com.example.jbenavides_pc.interactivestory.model;
+
+/**
+ * Created by jbenavides-pc on 10/29/2016.
+ */
+
+public class Choice {
+
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage){
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
